5e3c40804599282e254373a7bf1e0e86cfed27f4
Javier Pilco <brandon.pilco3401@utc.edu.ec>
2025-05-20 00:52:34 -0500

    Segundo commit

--- Aplicaciones/Control/models.py
+++ Aplicaciones/Control/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Libro(models.Model):
+    id = models.AutoField(primary_key=True)
+    codigo = models.CharField(max_length=20)
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=100)
+
+    def __str__(self):
+        fila = "{0}: {1} - {2}"
+        return fila.format(self.codigo, self.titulo, self.autor)
