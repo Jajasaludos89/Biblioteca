add7b1f7e5dfb6349062ae857bcf9f213457e1ec
Javier Pilco <brandon.pilco3401@utc.edu.ec>
2025-05-20 01:19:23 -0500

    Septimo commit

--- Aplicaciones/Control/views.py
+++ Aplicaciones/Control/views.py
@@ -44,3 +44,46 @@ def procesarEdicionLibro(request):
 
     messages.success(request, "Libro ACTUALIZADO exitosamente")
     return redirect('/')
+
+#Usuario
+
+def usuarios(request):
+    listadoUsuarios = Usuario.objects.all()
+    return render(request, "usuarios.html", {'usuarios': listadoUsuarios})
+
+def nuevoUsuario(request):
+    return render(request, "nuevoUsuario.html")
+
+def guardarUsuario(request):
+    nombre = request.POST["nombre"]
+    cedula = request.POST["cedula"]
+    direccion = request.POST["direccion"]
+
+    Usuario.objects.create(nombre=nombre, cedula=cedula, direccion=direccion)
+    messages.success(request, "Usuario GUARDADO exitosamente")
+    return redirect('/usuarios')
+
+def eliminarUsuario(request, id):
+    usuario = Usuario.objects.get(id=id)
+    usuario.delete()
+    messages.success(request, "Usuario ELIMINADO exitosamente")
+    return redirect('/usuarios')
+
+def editarUsuario(request, id):
+    usuario = Usuario.objects.get(id=id)
+    return render(request, "editarUsuario.html", {'usuarioEditar': usuario})
+
+def procesarEdicionUsuario(request):
+    id = request.POST["id"]
+    nombre = request.POST["nombre"]
+    cedula = request.POST["cedula"]
+    direccion = request.POST["direccion"]
+
+    usuario = Usuario.objects.get(id=id)
+    usuario.nombre = nombre
+    usuario.cedula = cedula
+    usuario.direccion = direccion
+    usuario.save()
+
+    messages.success(request, "Usuario ACTUALIZADO exitosamente")
+    return redirect('/usuarios')
