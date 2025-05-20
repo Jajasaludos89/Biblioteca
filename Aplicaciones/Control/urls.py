3364202b9c7852fb1942556763082fefd2fffed9
Javier Pilco <brandon.pilco3401@utc.edu.ec>
2025-05-20 02:15:38 -0500

    numero 12 commit

--- Aplicaciones/Control/urls.py
+++ Aplicaciones/Control/urls.py
@@ -2,12 +2,25 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Libros
     path('', views.inicio),
     path('nuevoLibro', views.nuevoLibro),
     path('guardarLibro', views.guardarLibro),
     path('eliminarLibro/<int:id>', views.eliminarLibro),
     path('editarLibro/<int:id>', views.editarLibro),
     path('procesarEdicionLibro', views.procesarEdicionLibro),
+
+    path('usuarios', views.usuarios),
+    path('nuevoUsuario', views.nuevoUsuario),
+    path('guardarUsuario', views.guardarUsuario),
+    path('eliminarUsuario/<int:id>', views.eliminarUsuario),
+    path('editarUsuario/<int:id>', views.editarUsuario),
+    path('procesarEdicionUsuario', views.procesarEdicionUsuario),
+
+    path('prestamos', views.prestamos),
+    path('nuevoPrestamo', views.nuevoPrestamo),
+    path('guardarPrestamo', views.guardarPrestamo),
+    path('eliminarPrestamo/<int:id>', views.eliminarPrestamo),
+    path('editarPrestamo/<int:id>', views.editarPrestamo),
+    path('procesarEdicionPrestamo', views.procesarEdicionPrestamo),
 
 ]
