243c4cccc8074b5e45b8aba50de9fc3f5b5502d6
Javier Pilco <brandon.pilco3401@utc.edu.ec>
2025-05-20 01:24:37 -0500

    Octavo commit

--- Aplicaciones/Control/views.py
+++ Aplicaciones/Control/views.py
@@ -87,3 +87,57 @@ def procesarEdicionUsuario(request):
 
     messages.success(request, "Usuario ACTUALIZADO exitosamente")
     return redirect('/usuarios')
+
+#Prestamos
+
+def prestamos(request):
+    listadoPrestamos = Prestamo.objects.all()
+    return render(request, "prestamos.html", {'prestamos': listadoPrestamos})
+
+def nuevoPrestamo(request):
+    libros = Libro.objects.all()
+    usuarios = Usuario.objects.all()
+    return render(request, "nuevoPrestamo.html", {'libros': libros, 'usuarios': usuarios})
+
+def guardarPrestamo(request):
+    usuario_id = request.POST["usuario"]
+    libro_id = request.POST["libro"]
+    fecha = request.POST["fecha_prestamo"]
+
+    usuario = Usuario.objects.get(id=usuario_id)
+    libro = Libro.objects.get(id=libro_id)
+
+    Prestamo.objects.create(usuario=usuario, libro=libro, fecha_prestamo=fecha)
+    messages.success(request, "Préstamo REGISTRADO exitosamente")
+    return redirect('/prestamos')
+
+def eliminarPrestamo(request, id):
+    prestamo = Prestamo.objects.get(id=id)
+    prestamo.delete()
+    messages.success(request, "Préstamo ELIMINADO exitosamente")
+    return redirect('/prestamos')
+
+def editarPrestamo(request, id):
+    prestamo = Prestamo.objects.get(id=id)
+    libros = Libro.objects.all()
+    usuarios = Usuario.objects.all()
+    return render(request, "editarPrestamo.html", {
+        'prestamoEditar': prestamo,
+        'libros': libros,
+        'usuarios': usuarios
+    })
+
+def procesarEdicionPrestamo(request):
+    id = request.POST["id"]
+    usuario_id = request.POST["usuario"]
+    libro_id = request.POST["libro"]
+    fecha = request.POST["fecha_prestamo"]
+
+    prestamo = Prestamo.objects.get(id=id)
+    prestamo.usuario = Usuario.objects.get(id=usuario_id)
+    prestamo.libro = Libro.objects.get(id=libro_id)
+    prestamo.fecha_prestamo = fecha
+    prestamo.save()
+
+    messages.success(request, "Préstamo ACTUALIZADO exitosamente")
+    return redirect('/prestamos')
